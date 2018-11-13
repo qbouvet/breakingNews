1e6e23deb11631792f7de9c33d90846a79a86894
Tobia Albergoni <tobia.albergoni@epfl.ch>
2018-11-13 21:27:18 +0100

    categorized dataset, events divided by class

--- scripts/download_clean_1_week.py
+++ scripts/download_clean_1_week.py
@@ -1,45 +1,112 @@
-from helpers import *
+from const import *
+from gdelt_dictionaries import *
+
 import pandas as pd
 import numpy as np
+import requests as req
+import os
+import zipfile
+import io
 
-## Date constants
-YEAR_MONTH = '201811'
-START_DAY = 5;
-MINUTES = ['00', '15', '30', '45']
 
-# Constants
-BASE_URL = 'http://data.gdeltproject.org/gdeltv2/'
-E = '.export'
-M = '.mentions'
-CSV = '.CSV'
-ZIP = '.zip'
+def download_data(url):
+	print('Dowloading %s' % url)
 
-# Keep track of week events to filter mentions
-events = np.ndarray([0,])
+	# Request ZIP
+	r = req.get(url)
 
-for d in ["%.2d" % (i + START_DAY) for i in range(7)]:
-    for h in ["%.2d" % i for i in range(24)]:
-        for m in MINUTES:
-        
-            # Define names
-            timestamp = YEAR_MONTH + d + h + m + '00'
-            E_filename = timestamp + E + CSV
-            E_url = BASE_URL + E_filename + ZIP
-            M_filename = timestamp + M + CSV 
-            M_url = BASE_URL + M_filename + ZIP
+	# Extract from ZIP
+	z = zipfile.ZipFile(io.BytesIO(r.content)) 
+	z.extractall()
 
-            # Download events
-            download_data(E_url)
-            E_df = read_event_csv(E_filename)
-            events = np.concatenate([events, E_df.GLOBALEVENTID.values])
+
+def delete_csv():
+	dir_name = "./"
+	test = os.listdir(dir_name)
+	
+	for item in test:
+		if item.endswith(".CSV"):
+			os.remove(os.path.join(dir_name, item))
+
+
+def read_event_csv(filename):
+	# Read csv file
+	E_df = pd.read_csv(filename, encoding='utf-8', sep='\t', header=None, usecols=E_cols)
+	E_df.columns = E_names
+	
+	# Drop nans
+	E_df = E_df.dropna(axis=0, subset=E_not_nan)
+	return E_df
+
+def read_mention_csv(filename, events):
+	# Read csv file
+	M_df = pd.read_csv(filename, encoding='utf-8', sep='\t', header=None, usecols=M_cols)
+	M_df.columns = M_names
+    
+	# Take only WEB mentions
+	M_df = M_df[M_df.MentionType == 1]
+
+	# Take only mentions of the event set (older ones are useless)
+	M_df = M_df[M_df.GLOBALEVENTID.isin(events)]
+    
+	return M_df  
+
+def to_json_events(filename, df):
+
+	# Get dfs for categories
+	df_classes = {k: v for k, v in df.groupby('Class')}
+	
+	for k, v in df_classes.items():
+		folder ='./../data/categorized/events/' +  QUADCLASS_FOLDERS[k] + '/'
+		json_name = folder + filename
+		v.to_json(json_name, orient='records')
+	
+
+def to_json_mentions(filename, df):
+
+	folder ='./../data/categorized/mentions/'
+    
+	# Write json
+	json_name = folder + filename
+	df.to_json(json_name, orient='records')
+	
+	
+def join_names(d, h, m):
+	t = YEAR_MONTH + d + h + m + '00'
+	E_f = t + E + CSV
+	E_u = BASE_URL + E_f + ZIP
+	M_f = t + M + CSV 
+	M_u = BASE_URL + M_f + ZIP
+	
+	return t, E_f, E_u, M_f, M_u
+	
+def iterate_week():
+	
+	# Keep track of week events to filter mentions
+	events = np.ndarray([0,])
+	
+	for d in ["%.2d" % (i + START_DAY) for i in range(7)]:
+		for h in ["%.2d" % i for i in range(24)]:
+			for m in MINUTES:
+
+				# Define names
+				timestamp, E_filename, E_url, M_filename, M_url = join_names(d, h, m)
+
+				# Download events
+				download_data(E_url)
+				E_df = read_event_csv(E_filename)
+				events = np.concatenate([events, E_df.ID.values])
+
+				# Download mentions
+				download_data(M_url)
+				M_df = read_mention_csv(M_filename, events)
+
+				# Write to json
+				to_json_events(timestamp + '.json', E_df)
+				to_json_mentions(timestamp + '.json', M_df)
+
+				# Clean directory
+				delete_csv()
+	
+iterate_week()
             
-            # Download mentions
-            download_data(M_url)
-            M_df = read_mention_csv(M_filename, events)
-            
-            # Write to json
-            to_json('E_' + timestamp + '.json', E_df)
-            to_json('M_' + timestamp + '.json', M_df)
-            
-# Clean directory
-delete_csv()
---
+++ scripts/gdelt_dictionaries.py
@@ -0,0 +1,61 @@
+from gdelt_event_codes import *
+
+QUADCLASS_CATEGORIES = {
+	1:'Verbal cooperation',
+	2:'Material cooperation',
+	3:'Verbal conflict',
+	4:'Material conflict'
+}
+
+QUADCLASS_FOLDERS = {
+	1:'VERBAL_COOPERATION',
+	2:'MATERIAL_COOPERATION',
+	3:'VERBAL_CONFLICT',
+	4:'MATERIAL_CONFLICT'
+}
+
+EVENT_CODE_CATEGORIES = {
+	1:'Make public statement',
+	2:'Appeal',
+	3:'Express intent to cooperate',
+	4:'Consult',
+	5:'Engage in diplomaticooperation',
+	6:'Engage in material cooperation',
+	7:'Provide aid',
+	8:'Yield',
+	9:'Investigate',
+	10:'Demand',
+	11:'Disapprove',
+	12:'Reject',
+	13:'Threaten',
+	14:'Protest',
+	15:'Exhibit force posture',
+	16:'Reduce relations',
+	17:'Coerce',
+	18:'Assault',
+	19:'Fight',
+	20:'Use unconventional mass violence'
+}
+
+EVENT_CODE_DICTS = {
+	1:MAKE_PUBLIC_STATEMENT,
+	2:APPEAL,
+	3:EXPRESS_INTENT_TO_COOPERATE,
+	4:CONSULT,
+	5:ENGAGE_IN_DIPLOMATIC_COOPERATION,
+	6:ENGAGE_IN_MATERIAL_COOPERATION,
+	7:PROVIDE_AID,
+	8:YIELD,
+	9:INVESTIGATE,
+	10:DEMAND,
+	11:DISAPPROVE,
+	12:REJECT,
+	13:THREATEN,
+	14:PROTEST,
+	15:EXHIBIT_MILITARY_POSTURE,
+	16:REDUCE_RELATIONS,
+	17:COERCE,
+	18:ASSAULT,
+	19:FIGHT,
+	20:ENGAGE_IN_UNCONVENTIONAL_MASS_VIOLENCE	
+}
