c0da67e7c59d84eb9aec91308ca1865b4346cfa7
gaumuel <gauthier.mueller@gmail.com>
2018-12-18 17:35:40 +0100

    modified ticks for x axes

--- scripts/displaySources.js
+++ scripts/displaySources.js
@@ -112,26 +112,10 @@ function display_source(timeseriesData, perSourceCumulativeCount, timestamps, so
     let sorted_data = Array.from(timeseriesData).map((x) => [x[0], sortMapByKeys(x[1])])
     let datepoints = Array.from(sorted_data[0][1].keys()).sort().map(d => d.slice(8,12))
 
-   // var xScale = d3.scaleLinear()
-   //      .domain([0, 100]) // input
-   //      .range([0, width - margin.left]); // output
-
-    var xScale = d3.scalePoint().rangeRound([0, width-20])
-    xScale.domain(datepoints);
-
-    let aaa = [
-                [
-                [[1,200],[20, 800], [30,500], [40,5], [50,400],[60,700], [70,100], [80,900]]],
-                [[[1,5],[20,30], [30,40], [40,5], [50,40],[60,50], [70,100], [80,10]]],
-                [[[1,700],[20,400], [30,400], [40,5], [50,400],[60,700], [70,900], [80,10]]],
-                [[[1,700],[20,400], [30,400], [40,5], [50,400],[60,700], [70,900], [80,10]]],
-                [[[1,200],[20, 200], [30,500], [40,5], [50,400],[60,300], [70,100], [80,500]]],
-             ]
-
-    let bb = chartdata.map(d => d[1].map(a => [a[0].slice(8,12), a[1]]))
+    let chart_data_prepared_to_plot = chartdata.map(d => d[1].map(a => [a[0].slice(8,12), a[1]]))
 
     for (var i = 0; i < 5; i++){
-        drawSingleChart([bb[i]], height, width, margin, xScale, i)    
+        drawSingleChart(chart_data_prepared_to_plot[i], height, width, margin, datepoints, i)    
     }
     // drawChart(timeseriesData, perSourceCumulativeCount)
 //===========================================================================================================
@@ -145,11 +129,14 @@ function display_source(timeseriesData, perSourceCumulativeCount, timestamps, so
 }
 
 
-function drawSingleChart(aa, height, width, margin, xScale, index){
+function drawSingleChart(aa, height, width, margin, datepoints, index){
 
-    let max_total_value = aa[0].map(d => d[1]).reduce((x, y) => ( x > y ? x : y ))
+    let max_total_value = aa.map(d => d[1]).reduce((x, y) => ( x > y ? x : y ))
     let digits_length = Math.log(max_total_value) * Math.LOG10E + 1 | 0;
     let x_axis_space = digits_length * 6
+
+    var xScale = d3.scalePoint().rangeRound([0, width-x_axis_space])
+        xScale.domain(datepoints);
 
   var yScale = d3.scaleLinear()
         .domain([0, max_total_value]) // input
@@ -168,11 +155,15 @@ function drawSingleChart(aa, height, width, margin, xScale, index){
       .append("g")
         .attr("transform", "translate(" + (margin.left + x_axis_space) + "," + (margin.top) + ")")
 
+
+    let datepoints_length = (Math.log(datepoints.length) * Math.LOG10E + 1 | 0)-1;
+    let ticks = xScale.domain().filter(function(d,i){ return !(i% (10**datepoints_length)); })            
+
    //  // 3. Call the x axis in a group tag
     svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + (height - margin.bottom) + ")")
-        .call(d3.axisBottom(xScale));
+        .call(d3.axisBottom(xScale).tickValues(ticks));
 
     // 4. Call the y axis in a group tag
     svg.append("g")
@@ -182,174 +173,20 @@ function drawSingleChart(aa, height, width, margin, xScale, index){
     console.log("aaaa: ", aa)
     // 9. Append the path, bind the data, and call the line generator
     svg.append("path")
-        .data(aa)
-        .attr("class", "line") // Assign a class for styling
-        .attr("d", line)//(d, z, z1) => (d3.line()(d.map( d => [xScale(d[0]))))))
-}
-
-function drawChart(timeseriesData, perSourceCumulativeCount) {
-
-    let max_total_value = Math.ceil(Array.from(perSourceCumulativeCount.values()).reduce((x, y) => ( x > y ? x : y )))
-    max_total_value = 1000
-
-
-    let digits_length = Math.log(max_total_value) * Math.LOG10E + 1 | 0;
-    let x_axis_space = digits_length * 6
-    // var dataset = d3.range(n).map(function(d) { return {"y": d3.randomUniform(1)() } })
-
-    var parentDiv = $(".sourcegraph-chart")[0];
-    var margin = {top: 10, right: 10, bottom: 10, left: 10}
-    var width = parentDiv.clientWidth - margin.left - margin.right;
-    var height = parentDiv.clientHeight - margin.top - margin.bottom;
-
-
-    let chartdata = [...timeseriesData.entries()].map( (entry) => {
-        return [entry[0], [...entry[1].entries()]]
-    })
-
-    let max_scale = Array.from(timeseriesData.values()).map(d => Array.from(d.values())).map(d => Math.max(...d))
-
-    let array_scales = []
-    for (var i = 0; i < 5; i++){
-        array_scales.push(make_scale(max_scale[i], height, margin.bottom))
-    }
-
-    console.log("max_scale: ", max_scale)
-    console.log("array_scales: ", array_scales)
-
-
-    // Sort Map(timestamp => count) by increasing order of timestamps for display in graph
-    let sorted_data = Array.from(timeseriesData).map((x) => [x[0], sortMapByKeys(x[1])])
-    let datepoints = Array.from(sorted_data[0][1].keys()).sort().map(d => d.slice(8,12))
-
-    var x = d3.scalePoint().rangeRound([0, width-x_axis_space])
-    x.domain(datepoints);
-
-   var yScale = d3.scaleLinear()
-        .domain([0, max_total_value]) // input
-        .range([height - margin.bottom, 0]); // output
-
-   var xScale = d3.scaleLinear()
-        .domain([0, 100]) // input
-        .range([0, width - margin.left]); // output
-
-   //  // 7. d3's line generator
-    var line = d3.line()
-        .x(function(d) { return xScale(d[0]); }) // set the x values for the line generator
-        .y(function(d) { return yScale(d[1]); }) // set the y values for the line generator
-        .curve(d3.curveMonotoneX) // apply smoothing to the line
-
-    var svg = d3.selectAll(".sourcegraph-chart")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + (margin.left + x_axis_space) + "," + (margin.top) + ")")
-
-   //  // 3. Call the x axis in a group tag
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + (height - margin.bottom) + ")")
-        .call(d3.axisBottom(xScale));
-
-    // 4. Call the y axis in a group tag
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(d3.axisLeft(yScale).ticks(4));
-
-
-    let bb = chartdata.map(d => d[1].map(a => [a[0].slice(8,12), a[1]]))
-
-    let aa = [
-                [[1,200],[20, 800], [30,500], [40,5], [50,400],[60,700], [70,100], [80,900]],
-                [[1,700],[20,400], [30,400], [40,5], [50,400],[60,700], [70,900], [80,10]],
-                [[1,700],[20,400], [30,400], [40,5], [50,400],[60,700], [70,900], [80,10]],
-             ]
-    console.log("aaaa: ", aa)
-    console.log("bbbbbb: ", bb)
-    // 9. Append the path, bind the data, and call the line generator
-    svg.append("path")
-        .data(aa)
+        .data([aa])
         .attr("class", "line") // Assign a class for styling
         .attr("d", line)//(d, z, z1) => (d3.line()(d.map( d => [xScale(d[0]))))))
 
-
+    svg.selectAll(".dot")
+    .data(aa).enter()
+    .append("circle")
+    .attr("class", "dot")
+    .attr("cx", (d) => {console.log("cx: ", d[0]); return xScale(d[0])})
+    .attr("cy", (d) => {console.log("cy: ", d[1]); return yScale(d[1])})
+    .attr("r", 3.5);
 }
 
 
-
-function test_line_chart(max_total_value, perSourceCumulativeCountarray_data){
-
-
-
-    let datepoints = Array.from(array_data[0].keys()).sort().map(d => d.slice(8,12))
-
-    console.log("datepoints: ", datepoints)
-    // datepoints = datepoints.map(d => d.slice(8,12))
-
-    var x = d3.scalePoint().rangeRound([0, width-x_axis_space])
-    x.domain(datepoints);
-    // 5. X scale will use the index of our data
-    var xScaleCategorical = d3.scaleOrdinal()
-        .domain(datepoints) // input
-        .range([0, width - x_axis_space]); // output
-
-    var xScale = d3.scaleLinear()
-        .domain([0,n-1]) // input
-        .range([0, width - x_axis_space]); // output
-
-   //  // 6. Y scale will use the randomly generate number
-    var yScale = d3.scaleLinear()
-        .domain([0, max_total_value]) // input
-        .range([height - margin.bottom, 0]); // output
-   //  // 7. d3's line generator
-
-    var line = d3.line()
-        .x(function(d, i) { console.log("ddddddddd", d[0]); return xScale(d[0]); }) // set the x values for the line generator
-        .y(function(d, i) { return yScale(d[1]); }) // set the y values for the line generator
-        .curve(d3.curveMonotoneX) // apply smoothing to the line
-
-    var svg = d3.selectAll(".sourcegraph-chart")
-        .append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-        .attr("transform", "translate(" + (margin.left + x_axis_space) + "," + (margin.top) + ")");
-
-   //  // 3. Call the x axis in a group tag
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + (height - margin.bottom) + ")")
-        .call(d3.axisBottom(x));
-
-    // 4. Call the y axis in a group tag
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(d3.axisLeft(yScale).ticks(4));
-
-    // 9. Append the path, bind the data, and call the line generator
-    svg.append("path")
-        .attr("class", "line") // Assign a class for styling
-        .attr("d", (d) => (d3.line()(make_tuples(d[1], x, yScale)))) // 11. Calls the line generator
-
-
-    svg.selectAll(".circle")
-      .data(svg.data()).enter().append("circle")
-        .attr("class", "dot")
-        .attr("cx", line.x())
-        .attr("cy", line.y())
-        .attr("r", 3.5);
-
-    // trying to put circles
-    // svg.selectAll(".visualize-source-mention-chart").append("circle")
-    //     .attr("class", "dot") // Assign a class for styling
-    //     .attr("cx", function(d, i) { return x(xCircle(d[1])[i]) })
-    //     .attr("cy", function(d, i) { return yScale(yCircle(d[1])[i]) })
-    //     .attr("r", 5);
-    // 12. Appends a circle for each datapoint
-
-}
-
- export {
+export {
     display_source, clean_sources
 }
